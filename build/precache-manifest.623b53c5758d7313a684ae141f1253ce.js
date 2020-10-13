5ceddb09afcb333388581bcb5d610c01fcf56036
rezriazi <sepehr541@gmail.com>
2020-10-13 11:17:28 -0700

    build added

---
+++ build/precache-manifest.623b53c5758d7313a684ae141f1253ce.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377958deae2ced79e3be312770d5b06d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00537d32d1aa632153f",
+    "url": "/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "63583d2eb18d2cb2956d",
+    "url": "/static/css/main.095073e9.chunk.css"
+  },
+  {
+    "revision": "a00537d32d1aa632153f",
+    "url": "/static/js/2.b90059d4.chunk.js"
+  },
+  {
+    "revision": "fc92056cb53d865d1ba7686863cb47b6",
+    "url": "/static/js/2.b90059d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63583d2eb18d2cb2956d",
+    "url": "/static/js/main.00bd8372.chunk.js"
+  },
+  {
+    "revision": "20e850d8d26677a0eb4e",
+    "url": "/static/js/runtime-main.da592674.js"
+  },
+  {
+    "revision": "c8076e0385265f440f8d5e5d136b7622",
+    "url": "/static/media/AboutMePic.c8076e03.jpeg"
+  },
+  {
+    "revision": "65235b1d7964820ae4e656c524ae9177",
+    "url": "/static/media/DesignBackground.65235b1d.jpg"
+  },
+  {
+    "revision": "0e8de080f1927040e8277221e5276bb5",
+    "url": "/static/media/PillUpDemo.0e8de080.mp4"
+  },
+  {
+    "revision": "43066bb0eaf362556e0ae97a411a415f",
+    "url": "/static/media/PlanetPanicDemo.43066bb0.mp4"
+  },
+  {
+    "revision": "112c47d2ae5a3349467e123dbdc80f8c",
+    "url": "/static/media/Planet_Panic_MainPic.112c47d2.png"
+  },
+  {
+    "revision": "525d163ede36bfccf0e7beac8031b7c3",
+    "url": "/static/media/maingamepicture.525d163e.jpg"
+  },
+  {
+    "revision": "c4cf497f4811153e1831055dfacd445b",
+    "url": "/static/media/portrait_logo_rez(new).c4cf497f.png"
+  },
+  {
+    "revision": "f91033dcbda07132f76de538294e2499",
+    "url": "/static/media/rezzmix.f91033dc.JPG"
+  },
+  {
+    "revision": "e0524739dd02007367d99e1055e191e1",
+    "url": "/static/media/toDoList.e0524739.png"
+  },
+  {
+    "revision": "fd068e91ef5a464e85cfb2eec2ddb357",
+    "url": "/static/media/ubctalentsized.fd068e91.jpg"
+  },
+  {
+    "revision": "7ea7726ac3197a89fc2ac17d7075f476",
+    "url": "/static/media/ubcwomeninsciencelogo_byRez.7ea7726a.jpg"
+  }
+]);
